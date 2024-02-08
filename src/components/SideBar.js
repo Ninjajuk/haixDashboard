6fa9daa5d64687ccaa893407301dad97167008ba
Samsu <biosamsuddin@gmail.com>
2024-02-08 17:37:08 +0530

    l

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -191,7 +191,7 @@ const AdminDash = () => {
                     <input
                       id="search"
                       name="search"
-                      className="block w-full rounded-md border border-transparent bg-gray-700 py-2 pl-10 pr-3 leading-5 text-gray-300 placeholder-gray-400 focus:border-white focus:bg-white focus:text-gray-900 focus:outline-none focus:ring-white sm:text-sm"
+                      className="block w-full rounded-md border border-transparent bg-gray-200 py-2 pl-10 pr-3 leading-5 text-gray-300 placeholder-gray-400 focus:border-white focus:bg-white focus:text-gray-900 focus:outline-none focus:ring-white sm:text-sm"
                       placeholder="Search"
                       type="search"
                     />
